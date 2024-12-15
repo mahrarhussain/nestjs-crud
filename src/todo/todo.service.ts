a80c579fb3907c95ad3f7879dcdeda7250b016e0
M Ahrar Hussain <mahrarhussain9@gmail.com>
2024-12-15 14:12:38 +0500

    Update todo.service.ts

--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -2,7 +2,51 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class TodoService {
+  private todos = [];
   createTodo(data): object {
-    return data;
+    const item = {
+      id: new Date().getTime(),
+      ...data,
+      createdAt: new Date().toISOString(),
+      isCompleted: false,
+    };
+
+    this.todos.push(item);
+    return {
+      msg: 'success',
+    };
+  }
+
+  getTodos() {
+    return this.todos;
+  }
+
+  updateTodo(data, id) {
+    const newTodo = this.todos.map((item, i) => {
+      if (item.id === Number(id)) {
+        return {
+          ...item,
+          ['title']: data.title,
+          isCompleted: true,
+        };
+      }
+      return item;
+    });
+
+    this.todos = newTodo;
+
+    return {
+      msg: 'todo updated',
+    };
+  }
+
+  delTodoById(id) {
+    const filteredTodos = this.todos.filter((t) => t.id !== Number(id));
+
+    this.todos = filteredTodos;
+
+    return {
+      msg: 'todo is deleted',
+    };
   }
 }
